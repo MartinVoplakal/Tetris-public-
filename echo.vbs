82fdcf05b9d57247571203836859b43d75a740a3
Martin Voplakal <77882075+MartinVoplakal@users.noreply.github.com>
2021-04-12 19:35:51 +0200

    Add files via upload

---
+++ echo.vbs
@@ -0,0 +1,1 @@
+MsgBox("Vitej ve hre Tetris. Budu rad za zpetnou vazbu na e-mail martin.air@seznam.cz")
---
+++ start.bat
@@ -0,0 +1,6 @@
+echo off
+echo.vbs
+java -jar Tetris.jar
+echo *
+echo dekuji ti za otestovani programu. Pokud se zde vypsaly nejake chyby (zacinaji Exception), nebo program prestal fungovat, posli mi obsah konzole na e-mail. Dekjuji...
+pause
